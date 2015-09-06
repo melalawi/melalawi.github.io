aaab02eaf711033ca1bbeb3dcc36f86b79fc4f12
insentience <elalawi_mohamed@yahoo.ca>
2015-09-06 02:41:07 -0500

    initial portfolio formatting, main.js

--- README.md
+++
@@ -1,1 +0,0 @@
-# melalawi.github.io
---
+++ js/main.js
@@ -0,0 +1,27 @@
+(function() {
+
+"use strict";
+	
+const DEFAULT_PAGE = "about";
+
+$(document).ready(function() {
+	$('.navButton').click(function() {
+        setPortfolioPage(this.id.replace('Button', ''));
+    });
+	
+	//set the default portfolio page
+	setPortfolioPage(DEFAULT_PAGE);
+});
+
+function setPortfolioPage(pageName) {
+    var buttons = $('.navButton'),
+		pages = $('.pageSegment');
+	
+	buttons.removeClass('active');
+	pages.css('display', 'none');
+	
+	buttons.filter('#' + pageName + 'Button').addClass('active');
+	pages.filter('#' + pageName + 'Page').css('display', '');
+}
+
+}());
